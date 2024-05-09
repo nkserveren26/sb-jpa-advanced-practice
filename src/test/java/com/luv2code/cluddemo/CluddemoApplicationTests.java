716ce0b6bb255100458cd1b89039c162331f522c
nkserveren26 <norinoritoshi@outlook.jp>
2024-05-09 23:25:12 +0900

    Spring Bootプロジェクト作成

---
+++ src/test/java/com/luv2code/cluddemo/CluddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.cluddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CluddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
