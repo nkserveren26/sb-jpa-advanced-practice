fb433e971974bd6bb1d548a43ade1c3ece89c990
nkserveren26 <norinoritoshi@outlook.jp>
2024-05-10 21:36:35 +0900

    DAOクラス作成

---
+++ src/main/java/com/luv2code/cluddemo/dao/AppDAO.java
@@ -0,0 +1,8 @@
+package com.luv2code.cluddemo.dao;
+
+import com.luv2code.cluddemo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+}
---
+++ src/main/java/com/luv2code/cluddemo/dao/AppDAOImpl.java
@@ -0,0 +1,24 @@
+package com.luv2code.cluddemo.dao;
+
+import com.luv2code.cluddemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AppDAOImpl implements AppDAO {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+}
