2152c8f61f0c05ad00d743d536e84c97c0fad869
nkserveren26 <norinoritoshi@outlook.jp>
2024-05-13 22:18:21 +0900

    InstructorDetailを削除する処理を追加

--- src/main/java/com/luv2code/cluddemo/CluddemoApplication.java
+++ src/main/java/com/luv2code/cluddemo/CluddemoApplication.java
@@ -24,8 +24,19 @@ public class CluddemoApplication {
 
 			//deleteInstructor(appDAO);
 
-			findInstructorDetail(appDAO);
+			//findInstructorDetail(appDAO);
+
+			deleteInstructorDetail(appDAO);
 		};
+	}
+
+	private void deleteInstructorDetail(AppDAO appDAO) {
+		int theId = 2;
+		System.out.println("Deleting instructor detail id: " + theId);
+
+		appDAO.deleteInstructorDetailById(theId);
+
+		System.out.println("Done!");
 	}
 
 	private void findInstructorDetail(AppDAO appDAO) {
