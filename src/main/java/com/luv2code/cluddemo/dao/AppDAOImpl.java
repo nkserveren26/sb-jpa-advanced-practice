b5fc81a420c69f9403a6a537af958f7917a58584
nkserveren26 <norinoritoshi@outlook.jp>
2024-05-18 22:10:58 +0900

    instructorとそれに紐づくcourseテーブルレコードを1つのqueryで取得する処理を追加

--- src/main/java/com/luv2code/cluddemo/dao/AppDAOImpl.java
+++ src/main/java/com/luv2code/cluddemo/dao/AppDAOImpl.java
@@ -81,4 +81,20 @@ public class AppDAOImpl implements AppDAO {
 
         return courses;
     }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int theId) {
+        // create query
+        TypedQuery<Instructor> query = entityManager.createQuery(
+                "select i from Instructor i "
+                    + "JOIN FETCH i.courses "
+                    + "where i.id = :data", Instructor.class
+        );
+
+        query.setParameter("data", theId);
+
+        Instructor instructor = query.getSingleResult();
+
+        return instructor;
+    }
 }
